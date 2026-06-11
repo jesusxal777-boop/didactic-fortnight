9ee046b8a05d27956f9872acd38eee04262e46ea
jesusxal777-boop <jesusxal777@gmail.com>
2026-06-11 16:03:34 -0500

    Update desktop.js

--- js/desktop.js
+++ js/desktop.js
@@ -1,1 +1,55 @@
-js
+const startBtn =
+document.getElementById("startBtn");
+
+const startMenu =
+document.getElementById("startMenu");
+
+startBtn.onclick=()=>{
+
+startMenu.classList.toggle("show");
+
+};
+
+document
+.querySelectorAll(".app-launch")
+.forEach(btn=>{
+
+btn.onclick=()=>{
+
+const app =
+btn.dataset.app;
+
+if(app==="notes")
+openNotes();
+
+if(app==="studio")
+openStudio();
+
+if(app==="files")
+openFiles();
+
+};
+
+});
+
+document
+.querySelectorAll(".desktop-icon")
+.forEach(icon=>{
+
+icon.onclick=()=>{
+
+const app =
+icon.dataset.app;
+
+if(app==="notes")
+openNotes();
+
+if(app==="studio")
+openStudio();
+
+if(app==="files")
+openFiles();
+
+};
+
+});
