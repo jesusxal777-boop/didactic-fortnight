381f3a8d9b49cfbd409958e0d57fee4a3cc0d855
jesusxal777-boop <jesusxal777@gmail.com>
2026-06-11 16:04:55 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function openNotes(){
+
+createWindow(
+"DreamByte Notes",
+
+`
+<textarea
+style="
+width:100%;
+height:100%;
+background:#111;
+color:white;
+"></textarea>
+`
+);
+
+}
+
+function openStudio(){
+
+createWindow(
+"DreamByte Studio",
+
+`
+<h2>DreamByte Studio Dev Edition</h2>
+
+<p>
+Aquí irá el editor HTML/CSS/JS.
+</p>
+`
+);
+
+}
+
+function openFiles(){
+
+createWindow(
+"DreamByte Files",
+
+`
+<h2>Explorador de Archivos</h2>
+`
+);
+
+}
